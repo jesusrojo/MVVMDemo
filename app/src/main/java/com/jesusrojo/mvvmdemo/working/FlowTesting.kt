dbeb9d2b322da1bcb3e64891a8d236404cc762e4
Jesús Rojo <virtualmjr-android@yahoo.com>
2021-09-01 16:57:44 -0300

    Testing flow

--- app/src/main/java/com/jesusrojo/mvvmdemo/working/FlowTesting.kt
+++ app/src/main/java/com/jesusrojo/mvvmdemo/working/FlowTesting.kt
@@ -8,30 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-
 //https://www.droidcon.com/news-detail?content-id=/repository/collaboration/Groups/spaces/droidcon_hq/Documents/public/news/android-news/Kotlin%20Flow%20on%20Android%20-%20Quick%20guide
-class FlowApiService(){
-    suspend fun fetchDatas(): List<UiData> = FakeUtil.getFakeListItemsOneTwo()
-}
-
-class FlowRepository(private val api: FlowApiService) {
-
-    fun fetchFlowDatas(): Flow<List<UiData>> {
-        return flow {
-            val fooList = api.fetchDatas()
-//                .map { fooFromApi ->    FooUI.fromResponse(fooFromApi)  }
-            emit(fooList)
-        }.flowOn(Dispatchers.IO) // Use the IO thread for this Flow
-    }
-
-//    fun getFooAlternative(): Flow<List<UiData>> {
-//        return api.fetchDatas()
-//            .map { /*fooFromApi -> FooUI.fromResponse(fooFromApi)*/}
-//            .asFlow()
-//            .flowOn(Dispatchers.IO)
-//    }
-}
-
 class FlowUiDataViewModel(val repository: FlowRepository) : ViewModel() {
 
     //
@@ -49,3 +26,29 @@ class FlowUiDataViewModel(val repository: FlowRepository) : ViewModel() {
         }
     }
 }
+
+
+class FlowRepository(private val api: FlowApiService) {
+
+    fun fetchFlowDatas(): Flow<List<UiData>> {
+        return flow {
+            val fooList = api.fetchDatas()
+//                .map { fooFromApi ->    FooUI.fromResponse(fooFromApi)  }
+            emit(fooList)
+        }.flowOn(Dispatchers.IO) // Use the IO thread for this Flow
+    }
+
+
+    class FlowApiService() {
+        suspend fun fetchDatas(): List<UiData> = FakeUtil.getFakeListItemsOneTwo()
+    }
+
+//    fun getFooAlternative(): Flow<List<UiData>> {
+//        return api.fetchDatas()
+//            .map { /*fooFromApi -> FooUI.fromResponse(fooFromApi)*/}
+//            .asFlow()
+//            .flowOn(Dispatchers.IO)
+//    }
+}
+
+
